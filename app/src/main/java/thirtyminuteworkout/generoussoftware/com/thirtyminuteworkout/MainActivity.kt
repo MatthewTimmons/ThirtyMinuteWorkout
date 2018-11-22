b5644a88912746887b80c0379b1fbcc5dd6a4a92
matthewtimmons <Matthwebt@yahoo.com>
2018-11-21 21:02:43 -0600

    Adds styles and colors. Sets up the main activity. Sets buttons to have placeholder toast messages.

--- app/src/main/java/thirtyminuteworkout/generoussoftware/com/thirtyminuteworkout/MainActivity.kt
+++ app/src/main/java/thirtyminuteworkout/generoussoftware/com/thirtyminuteworkout/MainActivity.kt
@@ -1,12 +1,32 @@
 package thirtyminuteworkout.generoussoftware.com.thirtyminuteworkout
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var thirtyMinuteWorkoutButton: Button
+    lateinit var customWorkoutButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Set views
+        thirtyMinuteWorkoutButton = findViewById(R.id.thirty_minute_workout_button)
+        customWorkoutButton = findViewById(R.id.custom_workout_button)
+
+        // TODO Set up intent to start next activity
+        thirtyMinuteWorkoutButton.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this@MainActivity, "This feature will be added later.", Toast.LENGTH_SHORT).show()
+        })
+
+        // TODO Set up custom workout options
+        customWorkoutButton.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this@MainActivity, "This feature will be added later.", Toast.LENGTH_SHORT).show()
+        })
+
     }
 }
